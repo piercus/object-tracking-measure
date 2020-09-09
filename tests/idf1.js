3d778bc9c5e2e628901e3e42c5dd285c66d3e8ec
Pierre Colle <piercus@gmail.com>
2020-09-09 09:23:57 +0200

    feat: idf1 is now calculated on all the computed and all true (instead of just using matches)

--- tests/idf1.js
+++ tests/idf1.js
@@ -44,8 +44,34 @@ test('idf1 on real-life example', t => {
 	const result = otm.idf1(options);
 	t.true(Math.abs(result - 1) > 1e-6);
 });
+
 test('idf1 on real-life example 2', t => {
 	const options = JSON.parse(fs.readFileSync('./tests/data/real-life2.json').toString());
 	const result = otm.idf1(options);
 	t.true(Math.abs(result - 1) > 1e-6);
+});
+
+test('idf1 on basic example', t => {
+	const trackGenerator = function (l, start = 1, end = 50, nullProb = 0.1) {
+		const range = end - start;
+		return new Array(l).fill(0).map(() => {
+			const isNull = Math.random() < nullProb;
+			if (isNull) {
+				return null;
+			}
+
+			return new Array(4).fill(0).map(() => Math.floor(Math.random() * range) + start);
+		});
+	};
+
+	const trackLength = 50;
+	const nTrack = 10;
+	const nTrackPredicted = 2;
+	const groundTruths = new Array(nTrack).fill(0).map(() => trackGenerator(trackLength));
+
+	const predictions = groundTruths.slice(0, nTrackPredicted).map(track => track.map(b => b && b.concat()));
+	const {idfp, idfn, idtp} = otm.idDetails({groundTruths, predictions});
+	t.is(idfp, 0);
+	t.is(idfn, groundTruths.slice(nTrackPredicted).map(track => track.filter(a => a).length).reduce((a, b) => a + b, 0));
+	t.is(idtp, predictions.map(track => track.filter(a => a).length).reduce((a, b) => a + b, 0));
 });
