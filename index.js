8dac9360f180813bd690b81d8149f40aa4e57889
Pierre Colle <piercus@gmail.com>
2021-11-19 17:03:21 +0100

    expose track-dist

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
 	motp: require('./lib/mot/motp'),
 	motDetails: require('./lib/mot/mot-details'),
 	idDetails: require('./lib/id/id-details'),
+	idTrackDist: require('./lib/id/track-dist'),
 	idf1: require('./lib/id/idf1')
 };
