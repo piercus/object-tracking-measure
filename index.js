9318e41ac9e6dbf3cf642f457c5a6105f0c99c2d
Pierre Colle <piercus@gmail.com>
2021-11-24 16:23:18 +0100

    add inpect functions

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ module.exports = {
 	motDetails: require('./lib/mot/mot-details'),
 	idDetails: require('./lib/id/id-details'),
 	idTrackDist: require('./lib/id/track-dist'),
-	idf1: require('./lib/id/idf1')
+	idf1: require('./lib/id/idf1'),
+	idInspect: require('./lib/id/inspect/inspect.js'),
+	motInspect: require('./lib/mot/inspect/inspect.js')
 };
