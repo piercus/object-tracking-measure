741a3350da7c5ba2128340ca356a1a186b3dce6d
marouaneamz <maroineamil99@gmail.com>
2022-07-13 09:15:13 +0200

    wpi piercus #27

--- tests/idf1-axe1.js
+++ tests/idf1-axe1.js
@@ -36,7 +36,7 @@ const groundTruths = [
 ];
 
 // Example 1 idf1 = 0.5 and ideucl = 0.37
-const predictions = [
+const predictionsA = [
 
 	[0,
 		26.25,
@@ -163,35 +163,151 @@ const predictions = [
 		600]
 ];
 
-/* Exemple 2 idf1 = 0.5 et ideucl = 0.67
-const predictions = [
-
-		[null, 26.25, 52.5, 78.75, 105, 130, 155, 180, 205,  230, 258.6666667, 287.3333333,
-		316, 344.6666667, 373.3333333,  null, null, null, null, null, null,
-null, null,null, null, null, null, null, null, null]
-,
-
-		[null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-null, 402, 421.3333333, 440.6666667 ,null, null, null, null,
-	null, null,null, null, null, null, null, null]
-,
-
-		[null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-			 null, null, null, 460, 474.2857143,
-			 488.5714286, 502.8571429, 517.1428571, 531.4285714,545.7142857, null, null, null, null, null]
-	,
-
-		[null, null, null, null, null, null, null, null, null, null, null, null, null, null,
-				null, null, null, null, null, null, null, null, null, null,null ,560, 570,580,590, 600
-			]
-];
-*/
-test('idf1 on simple example', t => {
+test('idf1 on track A', t => {
 	const result = otm.idf1({
 		groundTruths,
-		predictions,
+		predictions: predictionsA,
 		distFn: (a, b) => Math.abs(a - b),
 		threshold: 0.5
 	});
-	t.true(Math.abs(result - 0.5) < 1e-1); // Pour ideucl 0.67 exemple
+	t.true(Math.abs(result - 0.5) < 1e-1);
+});
+
+// Example 2 idf1 = 0.5 and ideucl = 0.67
+
+const predictionsB = [
+
+	[null,
+		26.25,
+		52.5,
+		78.75,
+		105,
+		130,
+		155,
+		180,
+		205,
+		230,
+		258.6666667,
+		287.3333333,
+		316,
+		344.6666667,
+		373.3333333,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null],
+
+	[null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		402,
+		421.3333333,
+		440.6666667,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null],
+
+	[null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		460,
+		474.2857143,
+		488.5714286,
+		502.8571429,
+		517.1428571,
+		531.4285714,
+		545.7142857,
+		null,
+		null,
+		null,
+		null,
+		null],
+
+	[null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		560,
+		570,
+		580,
+		590,
+		600]
+];
+
+test('idf1 on track B', t => {
+	const result = otm.idf1({
+		groundTruths,
+		predictions: predictionsB,
+		distFn: (a, b) => Math.abs(a - b),
+		threshold: 0.5
+	});
+	t.true(Math.abs(result - 0.5) < 1e-1);
 });
